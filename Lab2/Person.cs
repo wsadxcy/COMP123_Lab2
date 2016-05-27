3713f5ca661f1c4e13ceb7fd7ca84246b4df6d7d
Chenyuan Xie <1147719157xie@gmail.com>
2016-05-27 18:54:07 -0400

    Update person class include Name property

--- Lab2/Person.cs
+++ Lab2/Person.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
+    /**
+     * This class define a Person
+     * 
+     * @class Person
+     * @field {string} _name 
+     * @field {int} _age 
+     */
+
     public class Person
     {
+        // PRIVATE INSTANCE VARIABLES
+        private string _name;
+
+        // PUBLIC PROPERTIES
+        /**
+         * This is a public property for private _name field
+         * 
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        /**
+         * This is the default empty constructor for the Person class
+         * 
+         * 
+         * @Constructor Person
+         */
+        public Person()
+        {
+            this._name = "unknown name";
+        }
+
+        /**
+         * This is a constructor that takes name as a param and passes it to the _name Instance Variable 
+         *
+         * @Constructor Person
+         * @param {String}name
+         */
+        public Person(string name)
+        {
+            this._name = name;
+        }
     }
 }
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
+    /**
+     * This class is the driver class for the program
+     * 
+     * @class program
+     */
     public class Program
     {
+        /**
+         * the main method for my driver class
+         * 
+         * @method Main
+         * @param {sting[]} args
+         */
         public static void Main(string[] args)
         {
+            // create a new instance of the Person class
+            Person Nick = new Person("Nick");
+            // set a value in the Name property for the Person class
+            Nick.Name = "Nick";
+            // get the Name value and print to console
+            Console.WriteLine(Nick.Name);
+
         }
     }
 }
