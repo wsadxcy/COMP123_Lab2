12f015c0bb7b55d01aea56c08d1857bdbe3cda14
Chenyuan Xie <1147719157xie@gmail.com>
2016-05-27 16:27:06 -0400

    Initial Commit

---
+++ UnitTestForPersonClass/UnitTestForPersonClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForLab2
+{
+    [TestClass]
+    public class UnitTestForPersonClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
